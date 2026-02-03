a545da5f8bc1f725e2d6810a110ebd0a2fc1177a
aqueleeloi <133641583+aqueleeloi@users.noreply.github.com>
2026-02-03 04:28:33 +0000

    Create videos.js

---
+++ videos.js
@@ -0,0 +1,30 @@
+const tutorialData = [
+  {
+    "id": "kB0P7nXc1Lg",
+    "title": "Photoshop para Iniciantes",
+    "desc": "Interface e ferramentas básicas.",
+    "platform": "ps",
+    "tag": "INTRO"
+  },
+  {
+    "id": "jmsjrohHMVg",
+    "title": "Recorte Perfeito e Máscaras",
+    "desc": "Como remover fundos difíceis.",
+    "platform": "ps",
+    "tag": "RECORTE"
+  },
+  {
+    "id": "uzIhFgp3ezw",
+    "title": "Photopea: Visão Geral",
+    "desc": "Como usar o Photoshop gratuito no browser.",
+    "platform": "pp",
+    "tag": "INTRO"
+  },
+  {
+    "id": "dfP5CnCgkO0",
+    "title": "Máscaras no Photopea",
+    "desc": "Técnicas de recorte para a web.",
+    "platform": "pp",
+    "tag": "RECORTE"
+  }
+];
